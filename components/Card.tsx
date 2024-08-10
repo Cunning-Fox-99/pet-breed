f2379338ef655f5679a9bfa930eedc5f9c15e60a
Fox <kostya.moroz.96@gmail.com>
2024-08-10 16:23:18 +0300

    fixes

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,14 +1,24 @@
 import Link from 'next/link';
+import React from "react";
 
-const Card = ({ breed:any }) => {
+export interface Breed {
+    id: number;
+    name: string;
+    image?: {
+        url: string;
+    };
+    temperament?: string;
+}
 
+interface CardProps {
+    breed: Breed;
+}
+
+const Card: React.FC<CardProps> = ({ breed }) => {
     return (
-        <Link href={`/breed/${breed.id}`}>
-            <div className="border p-4 rounded-lg hover:shadow-lg transition">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src={breed.image?.url} alt={breed.name} className="w-full h-48 object-cover rounded-md" />
-                <h2 className="text-xl font-semibold mt-2">{breed.name}</h2>
-            </div>
+        <Link href={`/breed/${breed.id}`} className="block p-4 border rounded-lg shadow-md">
+            <img src={breed.image?.url} alt={breed.name} className="w-full h-40 object-cover rounded-md" />
+            <h2 className="mt-2 text-xl font-semibold">{breed.name}</h2>
         </Link>
     );
 };
